dbb594c9d986d1a2a681414e43a2a3b591147393
Adrian Sampson <adrian@radbox.org>
2021-03-16 15:07:43 -0400

    Wrap up a new State struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use tide::sse;
 use tera::Tera;
 use tide_tera::prelude::*;
+
+#[derive(Clone)]
+struct State {
+    tera: Tera,
+}
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
@@ -9,22 +13,25 @@ async fn main() -> tide::Result<()> {
     let mut tera = Tera::new("templates/**/*")?;
     tera.autoescape_on(vec!["html"]);
 
-    let mut app = tide::with_state(tera);
+    let mut app = tide::with_state(State {
+        tera: tera,
+    });
 
-    app.at("/chat").get(sse::endpoint(|_req, sender| async move {
+    app.at("/chat").get(tide::sse::endpoint(|req, sender| async move {
+        let _state = req.state();
         sender.send("message", "foo", None).await?;
         sender.send("message", "bar", None).await?;
         Ok(())
     }));
 
-    app.at("/").get(|req: tide::Request<Tera>| async move {
-        let tera = req.state();
+    app.at("/").get(|req: tide::Request<State>| async move {
+        let tera = &req.state().tera;
         tera.render_response("chat.html", &context! {})
     });
 
     app.at("/static").serve_dir("static/")?;
 
-    app.at("/send").post(|mut req: tide::Request<Tera>| async move {
+    app.at("/send").post(|mut req: tide::Request<State>| async move {
         let data: String = req.body_json().await?;
         println!("message: {}", data);
         Ok("")
