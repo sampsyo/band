336692f4012043ae5fb28e0dd762101c8fb70094
Adrian Sampson <adrian@radbox.org>
2021-03-16 14:22:23 -0400

    Add templates

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 use tide::sse;
+use tera::Tera;
+use tide_tera::prelude::*;
 
 #[async_std::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> tide::Result<()> {
     tide::log::start();
-    let mut app = tide::new();
+
+    let mut tera = Tera::new("templates/**/*")?;
+    tera.autoescape_on(vec!["html"]);
+
+    let mut app = tide::with_state(tera);
+
     app.at("/sse").get(sse::endpoint(|_req, sender| async move {
         sender.send("fruit", "banana", None).await?;
         sender.send("fruit", "apple", None).await?;
         Ok(())
     }));
+
+    app.at("/").get(|req: tide::Request<Tera>| async move {
+        let tera = req.state();
+        tera.render_response("chat.html", &context! {})
+    });
+
     app.listen("localhost:8080").await?;
     Ok(())
 }
