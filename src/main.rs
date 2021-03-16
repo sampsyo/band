7488f39584188c8d2d28e9076c1cf22ca348403f
Adrian Sampson <adrian@radbox.org>
2021-03-16 16:16:51 -0400

    Break out functions

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,31 @@ struct State {
     receiver: channel::Receiver<String>,
 }
 
+async fn chat_stream(req: tide::Request<State>, sender: tide::sse::Sender) -> tide::Result<()> {
+    let receiver = &req.state().receiver;
+    while let Ok(msg) = receiver.recv().await {
+        println!("recv'd {}", msg);
+        sender.send("message", msg, None).await?;
+    }
+
+    Ok(())
+}
+
+async fn chat_send(mut req: tide::Request<State>) -> tide::Result {
+    let data: String = req.body_json().await?;
+    println!("message: {}", data);
+
+    let sender = &req.state().sender;
+    sender.send(data).await?;
+
+    Ok(tide::Response::new(tide::StatusCode::Ok))
+}
+
+async fn chat_page(req: tide::Request<State>) -> tide::Result {
+    let tera = &req.state().tera;
+    tera.render_response("chat.html", &context! {})
+}
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     tide::log::start();
@@ -23,32 +48,10 @@ async fn main() -> tide::Result<()> {
         receiver: receiver,
     });
 
-    app.at("/chat").get(tide::sse::endpoint(|req: tide::Request<State>, sender| async move {
-        let receiver = &req.state().receiver;
-        while let Ok(msg) = receiver.recv().await {
-            println!("recv'd {}", msg);
-            sender.send("message", msg, None).await?;
-        }
-
-        Ok(())
-    }));
-
-    app.at("/").get(|req: tide::Request<State>| async move {
-        let tera = &req.state().tera;
-        tera.render_response("chat.html", &context! {})
-    });
-
+    app.at("/chat").get(tide::sse::endpoint(chat_stream));
+    app.at("/").get(chat_page);
     app.at("/static").serve_dir("static/")?;
-
-    app.at("/send").post(|mut req: tide::Request<State>| async move {
-        let data: String = req.body_json().await?;
-        println!("message: {}", data);
-
-        let sender = &req.state().sender;
-        sender.send(data).await?;
-
-        Ok("ok")
-    });
+    app.at("/send").post(chat_send);
 
     app.listen("localhost:8080").await?;
     Ok(())
